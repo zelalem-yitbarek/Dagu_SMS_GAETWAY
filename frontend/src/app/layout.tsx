ed7ef3d4b4bd8d8b5a2c669ba37754179da46dd1
Zelalem Yitbarek <zelalemyitbarek575@gmail.com>
2026-06-20 22:48:02 +0300

    feat:Gitignore File Intialized

---
+++ frontend/src/app/layout.tsx
@@ -0,0 +1,29 @@
+
+import "./globals.css"
+import { ThemeProvider } from "next-themes"
+
+export const metadata = {
+  title: "Dagu",
+  description: "SMS Service",
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  )
+}
